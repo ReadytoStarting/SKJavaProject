b055be6bb4bd9c3b29bc89dc11612ddb32856fec
ReadytoStarting <minnie092936@gmail.com>
2025-04-24 02:18:11 +0900

    hello

---
+++ src/Hello_World.java
@@ -0,0 +1,11 @@
+
+/*
+ * javac Hello_World.java
+ * java Hello_World
+ * Run ¥‹√‡≈∞: ctrl + f11
+ */
+public class Hello_World {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
